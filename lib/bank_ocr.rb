f84c06d94b5c2449b797fcb2ded0047c4efd5d46
Tiffanie Wong <tlwong1993@hotmail.com>
2023-11-21 12:59:23 -0700

    first comit

--- lib/bank_ocr.rb
+++ lib/bank_ocr.rb
@@ -1,3 +1,12 @@
 module BankOcr
-  # ...
+  def self.file_parser(file_content)
+    # file_content
+    lines = file_content.split("/n")
+    account_number_lines = lines[0..2]
+    pp account_number_lines
+  end
+
+  def self.read_file(file_path)
+    File.read(file_path)
+  end
 end
--- spec/bank_ocr_spec.rb
+++ spec/bank_ocr_spec.rb
@@ -6,4 +6,25 @@ describe BankOcr do
   it "loads fixtures" do
     expect(fixture("use_case_1")).to match(/123456789\n\z/)
   end
+
+  context 'when given a valid param' do
+    it "returns the parsed account number" do
+      file_content = "something"
+      expect(BankOcr.file_parser(file_content)).to eq("something")
+    end
+  end
+
+  context "when parsing a valid ORC entry" do
+    it "returns the correct account number" do
+      ocr_entry = "something"
+      expect(BankOcr.file_parser(ocr_entry)).to eq("something")
+
+    end
+  end
+
+  context "when parsing a valid OCR file" do
+    xit "returns an array of parsed account numbers" do
+    # stub in file content? - how should data be formatted?
+    end
+  end
 end
